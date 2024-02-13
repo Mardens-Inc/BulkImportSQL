d8770a4a56411ea30ae19d02ccfcab1a3a64fb2d
drew-chase <dcmanproductions@gmail.com>
2024-02-13 12:56:03 -0500

    Add SQLManager class to BulkImportSQL
    A new file, SQLManager.cs, has been created within the BulkImportSQL/sql directory. This file includes the definition for the SQLManager class, which currently contains a method prototype for establishing a connection to the database server.

---
+++ BulkImportSQL/sql/SQLManager.cs
@@ -0,0 +1,16 @@
+namespace BulkImportSQL.sql;
+
+public sealed class SQLManager
+{
+    private SQLManager()
+    {
+    }
+
+
+    public static bool Connect(string server, string database, string username, string password)
+    {
+        // Connect to the server
+
+        return false;
+    }
+}
