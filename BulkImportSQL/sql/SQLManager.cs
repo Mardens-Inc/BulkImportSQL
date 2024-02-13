b408efad8ef6cf0cb0b9ae5e9dd274de505663e9
drew-chase <dcmanproductions@gmail.com>
2024-02-13 13:48:56 -0500

    Update SQLManager with connection and disposal implementation
    The SQLManager class is updated to include new properties Server, Database, Username, and Password. It is now implementing IDisposable and IAsyncDisposable interfaces. The Connect method has been revised to use these new properties and create a MySqlConnection instance. Dispose and DisposeAsync methods are also introduced for proper clean-up.

--- BulkImportSQL/sql/SQLManager.cs
+++ BulkImportSQL/sql/SQLManager.cs
@@ -1,16 +1,39 @@
+using MySql.Data.MySqlClient;
+
 namespace BulkImportSQL.sql;
 
-public sealed class SQLManager
+public sealed class SqlManager : IDisposable, IAsyncDisposable
 {
-    private SQLManager()
+    public string Server { get; }
+    public string Database { get; }
+    public string Username { get; }
+    public string Password { get; }
+    private MySqlConnection Connection { get; }
+
+    private SqlManager(string server, string database, string username, string password)
     {
+        Server = server;
+        Database = database;
+        Username = username;
+        Password = password;
+
+        Connection = new MySqlConnection($"Server={Server};Database={Database};Uid={Username};Pwd={Password};");
     }
 
 
-    public static bool Connect(string server, string database, string username, string password)
+    public static bool Connect(string server, string database, string username, string password, out SqlManager manager)
     {
-        // Connect to the server
+        manager = new SqlManager(server, database, username, password);
+        return manager.Connection.State == System.Data.ConnectionState.Open;
+    }
 
-        return false;
+    public void Dispose()
+    {
+        Connection.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Connection.DisposeAsync();
     }
 }
