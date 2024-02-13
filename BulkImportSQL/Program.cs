869d8d547ee3a0a4fe8bad0ba6d5637a2abfd7b9
drew-chase <dcmanproductions@gmail.com>
2024-02-13 13:48:35 -0500

    Add SQL connection functionality to BulkImportSQL
    The main function now tries to connect to the SQL server using the username, server, database, and password provided in the argument fields. Successful or unsuccessful connection attempts are handled with appropriate console messages and a program exit in case of failure.

--- BulkImportSQL/Program.cs
+++ BulkImportSQL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BulkImportSQL.cli;
+using BulkImportSQL.sql;
 
 namespace BulkImportSQL;
 
@@ -7,5 +8,24 @@ public static class Program
     public static void Main()
     {
         ArgumentFields fields = CommandLine.Build();
+        if (SqlManager.Connect(fields.Server, fields.Database, fields.Username, fields.Password, out SqlManager manager))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Successfully connected to the server {fields.Server} with the database {fields.Database} using the username {fields.Username} and the password provided.");
+            Console.ResetColor();
+
+
+
+
+            // Dispose of the SQLManager instance
+            manager.Dispose();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"Unable to connect to the server {fields.Server} with the database {fields.Database} using the username {fields.Username} and the password provided.");
+            Console.ResetColor();
+            Environment.Exit((byte)ExitCodes.InvalidArguments);
+        }
     }
 }
