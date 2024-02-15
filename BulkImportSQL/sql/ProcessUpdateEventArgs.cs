a33b6a059806f8a77ba67164e8ade9678af8d91c
drew.chase <dcmanproductions@gmail.com>
2024-02-15 10:51:11 -0500

    Add 'Failed' property to ProcessUpdateEventArgs
    A 'Failed' property has been added to the ProcessUpdateEventArgs class, designed to hold the count of failed processes during a bulk import operation. This will provide more detailed information about the process outcome.

--- BulkImportSQL/sql/ProcessUpdateEventArgs.cs
+++ BulkImportSQL/sql/ProcessUpdateEventArgs.cs
@@ -3,6 +3,7 @@ namespace BulkImportSQL.sql;
 public class ProcessUpdateEventArgs
 {
     public required int Processed { get; init; }
+    public int Failed { get; init; }
     public required int Total { get; init; }
     public float Percentage => (float)Processed / Total;
     public required string State { get; init; }
